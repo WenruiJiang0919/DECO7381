d1c351d863e12e80e8d81fab20828ca7162487b4
Kai (Daniel) Wang <143713412+Kai-Daniel@users.noreply.github.com>
2023-10-17 21:09:21 +1000

    Update RTSUtils.cs

--- RTSUtils.cs
+++ RTSUtils.cs
@@ -1,3 +1,5 @@
+//source：https://www.bilibili.com/video/BV1tB4y1J7ra/?spm_id_from=333.1007.top_right_bar_window_custom_collection.content.click&vd_source=e81b410a72e94322b76cd65e6b58772b
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -40,4 +42,3 @@ public class RTSUtils : MonoBehaviour
         }
     }
 }
-//source：https://www.bilibili.com/video/BV1tB4y1J7ra/?spm_id_from=333.1007.top_right_bar_window_custom_collection.content.click&vd_source=e81b410a72e94322b76cd65e6b58772b
